6243d58e2c3c8625f6ddb9396ebf21e0306422c5
rash8907 <82785238+rash8907@users.noreply.github.com>
2022-11-07 15:38:56 -0700

    color update

--- sketch.js
+++ sketch.js
@@ -7,12 +7,18 @@
 // }
 
 var brushSize = 50;
+let r, g, b, c, m;
+
 
 function setup() {
-  createCanvas(710, 400);
-  background(102);
-  background('#fae');
+  createCanvas(1400, 700);
+  //background(102);
+  background('#black');
   describe('canvas with pink background');
+
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
@@ -21,4 +27,38 @@ function draw() {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
   cursor(ARROW, 50, 50);
+  background(127);
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(360, 200, 200, 200);
+  textSize(30);
+  textWrap(WORD);
+  text('PRESS ME', 280, 60, 200);
+  
+  ellipse(300, 500, 500, 500);
+  ellipse(500, 500, 300, 300);
+  ellipse(800, 300, 400, 400);
+  ellipse(950, 400, 350, 350);
+  ellipse(1200, 600, 350, 350);
+  //(x, y, w, h)
+
+
+
 }
+
+// When the user clicks the mouse
+function mousePressed() {
+  // Check if mouse is inside the circle
+  let d = dist(mouseX, mouseY, 360, 200);
+  if (d < 100) {
+    // Pick new random color values
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    c = random(255);
+    m = random(255);
+  }
+}
+
